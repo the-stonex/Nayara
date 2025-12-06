c7af9dd477dbcdbc11b62661fd259b7ced25876b
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2025-12-06 10:29:33 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -11,10 +11,10 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.logging import LOGGER
-from AnonXMusic.platforms._httpx import HttpxClient
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from AviaxMusic.logging import LOGGER
+from AviaxMusic.platforms._httpx import HttpxClient
+from AviaxMusic.utils.database import is_on_off
+from AviaxMusic.utils.formatters import time_to_seconds
 from config import API_URL, API_KEY
 
 class YouTubeUtils:
@@ -437,3 +437,4 @@ class YouTubeAPI:
             downloaded_file = await loop.run_in_executor(None, audio_dl)
         return downloaded_file, direct
             
+
